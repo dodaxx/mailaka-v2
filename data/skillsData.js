fe9f7295049e9b07b695c513c5589a106e2bd8c9
dodaxx <fenosoaramaroson9@gmail.com>
2023-09-25 14:01:47 +0300

    data skills

--- data/skillsData.js
+++ data/skillsData.js
@@ -1,6 +1,34 @@
 import HmtlIcon from "components/icons/HmtlIcon.vue";
 import CssIcon from "components/icons/CssIcon.vue";
-  
+import JsIcon from "components/icons/JsIcon.vue";
+import VueIcon from "components/icons/VueIcon.vue";
+import NuxtIcon from "components/icons/NuxtIcon.vue";
+import ReactIcon from "components/icons/ReactIcon.vue";
+import NextIcon from "components/icons/NextIcon.vue";
+import FlutterIcon from "components/icons/FlutterIcon.vue";
+import TailwindIcon from "components/icons/TailwindIcon.vue";
+import BootstrapIcon from "components/icons/BootstrapIcon.vue";
+import SassIcon from "components/icons/SassIcon.vue";
+import NodeIcon from "components/icons/NodeIcon.vue";
+import TypescriptIcon from "components/icons/TypescriptIcon.vue";
+import GolangIcon from "components/icons/GolangIcon.vue";
+import DartIcon from "components/icons/DartIcon.vue";
+import PythonIcon from "components/icons/PythonIcon.vue";
+import DjangoIcon from "components/icons/DjangoIcon.vue";
+import phpIcon from "components/icons/phpIcon.vue";
+import WordpressIcon from "components/icons/WordpressIcon.vue";
+import MysqlIcon from "components/icons/MysqlIcon.vue";
+import PostgresIcon from "components/icons/PostgresIcon.vue";
+import GsapIcon from "components/icons/GsapIcon.vue";
+import SwiperIcon from "components/icons/SwiperIcon.vue";
+import AnimeJsIcon from "components/icons/AnimeJsIcon.vue";
+import ChartJsIcon from "components/icons/ChartJsIcon.vue";
+import GitIcon from "components/icons/GitIcon.vue";
+import GithubIcon from "components/icons/GithubIcon.vue";
+import GitlabIcon from "components/icons/GitlabIcon.vue";
+import FigmaIcon from "components/icons/FigmaIcon.vue";
+import adobeXdIcon from "components/icons/adobeXdIcon.vue";
+
 export default skillsData = [
   {
     basic: [
@@ -10,19 +38,131 @@ export default skillsData = [
       },
       {
         label: "Css",
-        icon: Css,
+        icon: CssIcon,
       },
       {
-        label: "HTML",
-        icon: Html,
+        label: "Javascript",
+        icon: JsIcon,
       },
       {
-        label: "HTML",
-        icon: Html,
+        label: "Vue.Js",
+        icon: VueIcon,
       },
       {
-        label: "HTML",
-        icon: Html,
+        label: "Nuxt.js",
+        icon: NuxtIcon,
+      },
+      {
+        label: "React.js",
+        icon: ReactIcon,
+      },
+      {
+        label: "Next.js",
+        icon: NextIcon,
+      },
+      {
+        label: "Flutter",
+        icon: FlutterIcon,
+      },
+      {
+        label: "Wordpress",
+        icon: WordpressIcon,
+      },
+    ],
+    framework: [
+      {
+        label: "Tailwind.css",
+        icon: TailwindIcon,
+      },
+      {
+        label: "Bootstrap",
+        icon: BootstrapIcon,
+      },
+      {
+        label: "Sass",
+        icon: SassIcon,
+      },
+    ],
+    back: [
+      {
+        label: "Node.js",
+        icon: NodeIcon,
+      },
+      {
+        label: "Typescript",
+        icon: TypescriptIcon,
+      },
+      {
+        label: "Golang",
+        icon: GolangIcon,
+      },
+      {
+        label: "Dart",
+        icon: DartIcon,
+      },
+      {
+        label: "Python",
+        icon: PythonIcon,
+      },
+      {
+        label: "Django",
+        icon: DjangoIcon,
+      },
+      {
+        label: "Php",
+        icon: phpIcon,
+      },
+    ],
+    bd: [
+      {
+        label: "Mysql",
+        icon: MysqlIcon,
+      },
+      {
+        label: "Postgres",
+        icon: PostgresIcon,
+      },
+    ],
+    lib: [
+      {
+        label: "Gsap",
+        icon: GsapIcon,
+      },
+      {
+        label: "Swiper",
+        icon: SwiperIcon,
+      },
+      {
+        label: "Anime.js",
+        icon: AnimeJsIcon,
+      },
+      {
+        label: "Chart.js",
+        icon: ChartJsIcon,
+      },
+    ],
+    versionning: [
+      {
+        label: "Git",
+        icon: GitIcon,
+      },
+      {
+        label: "Github",
+        icon: GithubIcon,
+      },
+      {
+        label: "Gitlab",
+        icon: GitlabIcon,
+      },
+    ],
+    design: [
+      {
+        label: "Figma",
+        icon: FigmaIcon,
+      },
+      {
+        label: "AdobeXD",
+        icon: adobeXdIcon,
       },
     ],
   },
